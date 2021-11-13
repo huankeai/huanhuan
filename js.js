5611e4356024397d7241a8851800cd5434349685
huankeai <94215769+huankeai@users.noreply.github.com>
2021-11-13 15:07:40 +0800

    万熊哥

---
+++ js.js
@@ -0,0 +1,7 @@
+{/* <script language="javascript">
+function changeColor(){
+var color="#f00|#0f0|#00f|#880|#808|#088|yellow|green|blue|gray";
+color=color.split("|");
+document.getElementById("blink").style.color=color[parseInt(Math.random() * color.length)];
+
+setInterval("changeColor()",200); */}
